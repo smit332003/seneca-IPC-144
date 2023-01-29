f70c69f3301a4031cf8bb919d04480b9779330c7
Smit Patel <68802171+smit332003@users.noreply.github.com>
2023-01-29 06:37:51 -0500

    Add files via upload

---
+++ core.h
@@ -0,0 +1,22 @@
+/*
+*****************************************************************************
+                        Assignment 1 - Milestone 1
+Full Name  : Smitkumar K Patel
+Student ID#: 157308214
+Email      : spatel556@myseneca.ca
+Section    : NCC
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+void clearInputBuffer(void);
+void suspend(void);
+int inputInt();
+int inputIntPositive();
+int  inputIntRange(int, int);
+char inputCharOption (const char*);
+void inputCString(char *,int,int);
+void displayFormattedPhone (const char *);
